12a5891c1cf24f836a6feb08043884af9e067869
Brendan Gordon <brendan.gordon@gmail.com>
2015-03-06 00:50:30 +0000

    Initial commit with POC code

---
+++ README.md
@@ -0,0 +1,4 @@
+
+Welcome to Data.gov.au Here.
+
+Search what's on data.gov.au at your address, so you can see the data that matters to you.
---
+++ client/js/controller.js
@@ -0,0 +1,14 @@
+var datahereApp = angular.module('datahereApp', []);
+
+datahereApp.controller('GFICtrl', function ($scope) {
+  $scope.results = [
+    {'name': 'MyBroadband',
+     'properties':[
+        {
+            'ESA_NAME':'UNANDERRAAAA'
+            ,'ESA_DA' : 'UNAN:BQP34419'
+        }
+        ]
+    },
+  ];
+});
